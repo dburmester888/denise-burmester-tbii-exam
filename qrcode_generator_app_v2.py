6538ded3742dfa0a3ab11a1d45f77041a0c07267
dburmester888 <denise.burmester@stud.leuphana.de>
2024-11-12 12:46:21 +0100

    Update qrcode_generator_app_v2.py

--- qrcode_generator_app_v2.py
+++ qrcode_generator_app_v2.py
@@ -1,18 +1,18 @@
 import streamlit as st
-from decode_qrcode
+from generate_qrcode_page import generate_qrcode_page
 
-st. sgt_page_config(
-    sgt.file= "QR Code Generator v2",
-    sgt.img= "💫"
-)
+# set up the page
+st.set_page_config(page_title="My QR Code app",
+                   page_icon="😊")
 
 #directs a side bar with different pages
-options =[`Generate QR CODE', `About me`']
-page_selection = st.sidebar.selection ("Menu", options)
+options =['Generate QR CODE', "Decode QR Code", 'About me']
+page_selection = st.sidebar.selectbox("Menu", options)
 
 #
-if page_selection == "Generate QR code":
-    st.title("Generate QR Code")
-elif page_selectionn == "Decode QR Code":
+if page_selection == "Generate QR CODE":
+    generate_qrcode_page()
+elif page_selection == "Decode QR Code":
     st.write("We will work on this")
-elif page_selection == "About me"
+elif page_selection == "About me":
+    st.write("My name is Denise 😊")
